0c3ed3486f268b93d2c920a7ffdfabcde2a48b71
Nobuhiro Kobayashi <mozx@mozx.org>
2021-01-15 11:31:04 +0900

    added calendar navigation implementation

--- src/components/mcalendar.js
+++ src/components/mcalendar.js
@@ -1,11 +1,83 @@
+var mcalendarnav = {
+  props: {
+    startyear: { type: Number, required: true }
+  },
+  template: '' +
+    '<div class="mcalendar-nav">' +
+      '<div>' +
+        '<b-button @click="decrement">Prev</b-button>' +
+        '<b-form-select ' +
+          'v-model="year" ' +
+          ':options="yearoptions" ' +
+          '@change="changed">' +
+        '</b-form-select>' +
+        '<b-form-select ' +
+          'v-model="month" ' +
+          ':options="monthoptions" ' +
+          '@change="changed">' +
+        '</b-form-select>' +
+        '<b-button @click="increment">Next</b-button>' +
+      '</div>' +
+    '</div>',
+  data () {
+    return {
+      year: 2020,
+      month: 1,
+      yearoptions: [],
+      monthoptions: [],
+    }
+  },
+  mounted () {
+    const today = new Date();
+    this.year = today.getFullYear();
+    this.month = today.getMonth() + 1;
+    this.yearoptions.length = 0;
+    for (var y = today.getFullYear(); y >= this.startyear; y -= 1) {
+      this.yearoptions.push({ value: y, text: y.toString() });
+    }
+    this.monthoptions.length = 0;
+    for (var m = 1; m <= 12; m += 1) {
+      this.monthoptions.push({ value: m, text: m.toString() });
+    }
+    this.changed();
+  },
+  methods: {
+    increment () {
+      this.month += 1;
+      if (this.month > 12) {
+        this.year += 1;
+        this.month -= 12;
+      }
+      this.changed();
+    },
+    decrement () {
+      if (this.year === this.startyear && this.month === 1) {
+        return;
+      }
+      this.month -= 1;
+      if (this.month < 1) {
+        this.year -= 1;
+        this.month += 12;
+      }
+      this.changed();
+    },
+    changed () {
+      console.log('year or month changed');
+    }
+  }
+};
+
 var mcalendar = {
   props: {
     startyear: { type: Number, required: true }
   },
   template: '' +
     '<div class="mcalendar">' +
-      '<div class="mcalendar-nav">Calendar Nav {{ startyear }}</div>' +
+      '<mcalendar-nav :startyear="startyear" class="mcalendar-nav"></mcalendar-nav>' +
       '<div class="mcalendar-table">Calendar Table</div>' +
-    '</div>'
+    '</div>',
+  components: {
+    'mcalendar-nav': mcalendarnav
+  }
 };
 
